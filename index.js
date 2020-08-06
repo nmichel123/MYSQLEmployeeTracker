857909eb42fbcdb911e51fd03f043eb4bb7e9fab
Nico Michel <nicomichel@utexas.edu>
2020-08-05 22:08:53 -0500

    got working questions and think that some answers are being pushed to server

--- index.js
+++ index.js
@@ -15,5 +15,118 @@ var connection = mysql.createConnection({
     database: "mysqlemployeetracker"
   });
 
- 
+  connection.connect(function(err) {
+    if (err) throw err;
+    displayOptions();
+  });
+  
+function displayOptions() {
+    inquirer
+      .prompt({
+        name: "action",
+        type: "list",
+        message: "What would you like to do?",
+        choices: [
+          "Add departments, roles, employees",
+          "View departments, roles, employees",
+          "Update employee roles",
+        ]
+      })
+      .then(function(answer) {
+        switch (answer.action) {
+        case "Add departments, roles, employees":
+          addInfo();
+          break;
+  
+        case "View departments, roles, employees":
+          viewInfo();
+          break;
+  
+        case "Update employee roles":
+          updateRoles();
+          break;
+        }
+      });
+  }
+
+  function addInfo() {
+    // prompt for info about the item being put up for auction
+    inquirer
+      .prompt([
+        {
+          name: "addWhat",
+          type: "list",
+          choices: [
+            "Add departments",
+            "Add roles",
+            "Add employees" ],
+          message: "What would you like to add?"
+        },
+      ])
+      .then(function(answer) {
+        switch (answer.addWhat) {
+        case "Add departments":
+          addDep();
+          break;
+  
+        case "Add roles":
+          addRole();
+          break;
+  
+        case "Add employees":
+          addEmp();
+          break;
+        }
+      });
+    }
+
+    function addDep () {
+        inquirer
+        .prompt([
+        {
+            name: "depName" ,
+            type: "input" ,
+            message: "What is the name of the new department?" ,
+        },
+        ])
+        .then(function(answer){
+            connection.query(
+                "INSERT INTO department SET ?",
+                {
+                    dep_name: answer.depName
+                },
+                function(err) {
+                    if (err) throw err;
+                    console.log("Your department was created successfully!");
+                    // re-prompt the user for if they want to bid or post
+                    displayOptions();
+                }
+            )
+        });
+    }
+
+    function addDep () {
+        inquirer
+        .prompt([
+        {
+            name: "depName" ,
+            type: "input" ,
+            message: "What is the name of the new department?" ,
+        },
+        ])
+        .then(function(answer){
+            connection.query(
+                "INSERT INTO department SET ?",
+                {
+                    dep_name: answer.depName
+                },
+                function(err) {
+                    if (err) throw err;
+                    console.log("Your department was created successfully!");
+                    // re-prompt the user for if they want to bid or post
+                    displayOptions();
+                }
+            )
+        });
+    }
   
